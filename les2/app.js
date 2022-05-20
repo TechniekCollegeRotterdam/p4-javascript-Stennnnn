342d80d17624e6e937aa9025f245d573e899791d
9017412@student.zadkine.nl <9017412@student.zadkine.nl>
2022-05-20 14:16:06 +0200

    oefening 2 - Move the bird - 2

--- les2/app.js
+++ les2/app.js
@@ -1,11 +1,33 @@
 //Oefening angry-birds
 // . is voor class, hashtag is voor id en niks is voor html tag.
-const bird = document.querySelector(".bird");
-// de image opgehaald uit html
-let pixels = 0;
-bird.addEventListener("click", function(){
-    pixels = pixels + 50;
-    bird.style.left = pixels + "px";
 
-});
-
+let bird = document.querySelector(".bird")
+let px = 0;
+window.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key == "ArrowRight") {
+        px = px + 50;
+        bird.style.left = px + "px";
+    }
+})
+window.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key == "ArrowLeft") {
+        px = px - 50;
+        bird.style.left = px + "px";
+    }
+})
+window.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key == "ArrowUp") {
+        px = px + 50;
+        bird.style.bottom = px + "px";
+    }
+})
+window.addEventListener("keydown", function (e) {
+    console.log(e.key);
+    if (e.key == "ArrowDown") {
+        px = px - 50;
+        bird.style.bottom = px + "px";
+    }
+})
